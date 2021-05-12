377946ab18a314a897338b6f6987b8c3e4efb993
Anniee <52150815+Anni1123@users.noreply.github.com>
2021-05-12 15:03:14 +0530

    FoldingCell

---
+++ FoldingCell/app/src/main/java/com/anni/foldingcell/MainActivity.java
@@ -0,0 +1,25 @@
+package com.anni.foldingcell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final com.ramotion.foldingcell.FoldingCell fc =  findViewById(R.id.folding_cell);
+//​
+        // attach click listener to folding cell
+        fc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fc.toggle(false);
+            }
+        });
+    }
+}
