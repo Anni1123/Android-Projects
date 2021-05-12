20903fa66fed91da64e23480b0b330346f5ec072
Anniee <52150815+Anni1123@users.noreply.github.com>
2021-05-12 14:57:32 +0530

    PatternLockView

---
+++ PatternLockView/app/src/main/java/com/anni/patternlockview/MainActivity.java
@@ -0,0 +1,80 @@
+package com.anni.patternlockview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
+import com.andrognito.patternlockview.utils.ResourceUtils;
+import com.andrognito.rxpatternlockview.RxPatternLockView;
+import com.andrognito.rxpatternlockview.events.PatternLockCompoundEvent;
+
+import java.util.List;
+
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends AppCompatActivity {
+
+    PatternLockView mPatternLockView;
+    private PatternLockViewListener mPatternLockViewListener = new PatternLockViewListener() {
+        @Override
+        public void onStarted() {
+            Log.d(getClass().getName(), "Pattern drawing started");
+        }
+
+        @Override
+        public void onProgress(List<PatternLockView.Dot> progressPattern) {
+            Log.d(getClass().getName(), "Pattern progress: " +
+                    PatternLockUtils.patternToString(mPatternLockView, progressPattern));
+        }
+
+        @Override
+        public void onComplete(List<PatternLockView.Dot> pattern) {
+            Log.d(getClass().getName(), "Pattern complete: " +
+                    PatternLockUtils.patternToString(mPatternLockView, pattern));
+        }
+
+        @Override
+        public void onCleared() {
+            Log.d(getClass().getName(), "Pattern has been cleared");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mPatternLockView =  findViewById(R.id.pattern_lock_view);
+        mPatternLockView.addPatternLockListener(mPatternLockViewListener);
+        mPatternLockView.setDotCount(3);
+        mPatternLockView.setDotNormalSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_size));
+        mPatternLockView.setDotSelectedSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_selected_size));
+        mPatternLockView.setPathWidth((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_path_width));
+        mPatternLockView.setAspectRatioEnabled(true);
+        mPatternLockView.setNormalStateColor(ResourceUtils.getColor(this, R.color.white));
+        mPatternLockView.setWrongStateColor(ResourceUtils.getColor(this, R.color.pomegranate));
+        mPatternLockView.setDotAnimationDuration(150);
+        mPatternLockView.setPathEndAnimationDuration(100);
+
+        RxPatternLockView.patternChanges(mPatternLockView)
+                .subscribe(new Consumer<PatternLockCompoundEvent>() {
+                    @Override
+                    public void accept(PatternLockCompoundEvent event) throws Exception {
+                        if (event.getEventType() == PatternLockCompoundEvent.EventType.PATTERN_STARTED) {
+                            Log.d(getClass().getName(), "Pattern drawing started");
+                        } else if (event.getEventType() == PatternLockCompoundEvent.EventType.PATTERN_PROGRESS) {
+                            Log.d(getClass().getName(), "Pattern progress: " +
+                                    PatternLockUtils.patternToString(mPatternLockView, event.getPattern()));
+                        } else if (event.getEventType() == PatternLockCompoundEvent.EventType.PATTERN_COMPLETE) {
+                            Log.d(getClass().getName(), "Pattern complete: " +
+                                    PatternLockUtils.patternToString(mPatternLockView, event.getPattern()));
+                        } else if (event.getEventType() == PatternLockCompoundEvent.EventType.PATTERN_CLEARED) {
+                            Log.d(getClass().getName(), "Pattern has been cleared");
+                        }
+                    }
+                });
+    }
+}
