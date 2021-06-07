d87512598d6ffac3d8714d0e607d769ed1704660
Anniee <52150815+Anni1123@users.noreply.github.com>
2021-06-07 23:38:08 +0530

    Update MainActivity.java

--- ScrollImageView/app/src/main/java/com/anni/scrollimageview/MainActivity.java
+++ ScrollImageView/app/src/main/java/com/anni/scrollimageview/MainActivity.java
@@ -1,26 +1,37 @@
-package com.anni.scrollimageview;
+package com.prepare.makedirectory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.q42.android.scrollingimageview.ScrollingImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean moving1 =true,moving2=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ScrollingImageView scrollingBackground = (ScrollingImageView) findViewById(R.id.scrolling_background);
 
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        ScrollingImageView scrollingBackground = (ScrollingImageView) findViewById(R.id.scrolling_background);
-        ScrollingImageView scrollingBackground1 = (ScrollingImageView) findViewById(R.id.scrolling_background1);
         scrollingBackground.start();
-        scrollingBackground1.start();
+        scrollingBackground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if already moving then stop
+                if(moving1){
+                    moving1=false;
+                    scrollingBackground.stop();
+                    //else start moving
+                }else{
+                    moving1=true;
+                    scrollingBackground.start();
+                }
+
+            }
+        });
+
     }
 }
